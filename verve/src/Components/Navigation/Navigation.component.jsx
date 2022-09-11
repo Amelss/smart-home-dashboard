cde062f1dd3ee62402f3f0b0cbf48261c8eacc03
Amelss <ameleyka@gmail.com>
2022-09-11 21:33:07 +0100

    installed react router

--- verve/src/Components/Navigation/Navigation.component.jsx
+++ verve/src/Components/Navigation/Navigation.component.jsx
@@ -6,7 +6,7 @@ export default function Navigation() {
   
   return (
     <div className="bg-navPurple rounded-3xl w-24 h-screen xl:h-full">
-      <div className=''>
+      <nav className=''>
         <img src="./assets/verve-images/home.png" alt="home"  className='mx-auto mb-20 mt-14 w-7'/>
         <img src="./assets/verve-images/dashboard.png" alt="home" className='bg-dashboardBackground nav-images rounded-l-full w-10 '/>
         <img src="./assets/verve-images/lamp.png" alt="home" className='nav-images w-5' />
@@ -15,7 +15,7 @@ export default function Navigation() {
         <img src="./assets/verve-images/users.png" alt="home"className='nav-images w-5' />
         <img src="./assets/verve-images/levels.png" alt="home" className='nav-images w-5'/>
         <img src="./assets/verve-images/exit.png" alt="home" className='mx-auto mb-5 mt-40 xl:mt-24 2xl:mt-48 w-5'/>
-      </div>
+      </nav>
 
 
 
