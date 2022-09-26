56a70cb4e2fc7822d9242e040d2fee5638919e77
Amelss <ameleyka@gmail.com>
2022-09-26 22:12:47 +0100

    added power graph image

--- verve/src/Components/DevicesPanel/PowerGraph.jsx
+++ verve/src/Components/DevicesPanel/PowerGraph.jsx
@@ -2,6 +2,37 @@ import React from 'react'
 
 export default function PowerGraph() {
   return (
-    <div className='bg-purple-300'>PowerGraph</div>
-  )
+    <div className="">
+      <div className="flex justify-between p-5">
+        <h1 className="">Power Consumed</h1>
+        <div className="flex items-center">
+          <select
+            name="month"
+            id="month"
+            className="border-none rounded-lg h-7 w-24 text-xs pr-0 pt-0 pb-0 bg-search"
+          >
+            <option value="month" selected disabled hidden >Month</option>
+            <option value="jan">January</option>
+            <option value="feb">February</option>
+            <option value="march">March</option>
+            <option value="april">April</option>
+            <option value="may">May</option>
+            <option value="june">June</option>
+            <option value="july">July</option>
+            <option value="august">August</option>
+            <option value="september">September</option>
+            <option value="october">October</option>
+            <option value="november">November</option>
+            <option value="december">December</option>
+          </select>
+          <img
+            src="/assets/verve-images/arrow.png"
+            alt="arrow"
+            className="ml-5 bg-search  rounded-lg pb-2 pt-2 pl-[10px] pr-[10px]"
+          />
+        </div>
+      </div>
+      <img src="/assets/verve-images/graph.png" alt="power graph" className=' p-5 mx-auto'/>
+    </div>
+  );
 }
--- verve/src/Components/TemperaturePanel/Thermostat.jsx
+++ verve/src/Components/TemperaturePanel/Thermostat.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Thermostat() {
   return (
-    <div className='bg-blue-300 font-mulish'>Thermostat</div>
+    <div className='bg-blue-300 font-mulish'>Thermostat
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, ullam aspernatur voluptatum vitae esse perspiciatis officiis est at doloremque, voluptate laborum rem libero atque dolore fugit tenetur. Animi, iste. Neque sit ratione fugiat reprehenderit id unde eum magnam in eveniet inventore. Facilis incidunt maiores quaerat veritatis, aut eveniet? Exercitationem maiores inventore, incidunt necessitatibus odio aut iusto provident nostrum blanditiis voluptas laboriosam pariatur quod recusandae a voluptate autem dolorum, totam dolorem optio beatae unde, debitis dicta deleniti. Eos ratione fugit quae laboriosam. Commodi quaerat, iure nihil itaque laboriosam maiores nam quas earum aut quam nemo blanditiis velit, ex debitis, totam aspernatur.</p>
+    </div>
   )
 }
